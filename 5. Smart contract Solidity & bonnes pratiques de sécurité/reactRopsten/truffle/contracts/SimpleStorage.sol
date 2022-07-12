fb147fe2e21755f1290fc4fc2237d1aaaf34b5e1
CarmeloH <carmelo.hays@gmail.com>
2022-07-12 18:56:27 +0200

    React Ropsten TP

--- 5. Smart contract Solidity & bonnes pratiques de sécurité/reactRopsten/truffle/contracts/SimpleStorage.sol
+++ 5. Smart contract Solidity & bonnes pratiques de sécurité/reactRopsten/truffle/contracts/SimpleStorage.sol
@@ -4,11 +4,11 @@ pragma solidity >=0.4.22 <0.9.0;
 contract SimpleStorage {
   uint256 value;
 
-  function read() public view returns (uint256) {
+  function get() public view returns (uint256) {
     return value;
   }
 
-  function write(uint256 newValue) public {
+  function set(uint256 newValue) public {
     value = newValue;
   }
 }
